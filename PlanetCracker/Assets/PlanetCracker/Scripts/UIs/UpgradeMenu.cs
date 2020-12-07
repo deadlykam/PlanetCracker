89711132f59061472ab1d4421bae24f772ebfc92
Kamran Wali <kamran369mars@gmail.com>
2020-12-07 20:05:35 +0000

    Game Balanced

--- PlanetCracker/Assets/PlanetCracker/Scripts/UIs/UpgradeMenu.cs
+++ PlanetCracker/Assets/PlanetCracker/Scripts/UIs/UpgradeMenu.cs
@@ -2,6 +2,7 @@
 using PlanetCracker.ScriptableObjects.Managers;
 using PlanetCracker.ScriptableObjects.Others;
 using PlanetCracker.States;
+using TMPro;
 using UnityEngine;
 
 namespace PlanetCracker.UIs
@@ -13,10 +14,17 @@ namespace PlanetCracker.UIs
         [SerializeField] private int _health;
         [SerializeField] private int _dmg;
         [SerializeField] private int _speed;
+        [SerializeField] private TextMeshProUGUI _healthText;
+        [SerializeField] private TextMeshProUGUI _damageText;
+        [SerializeField] private TextMeshProUGUI _speedText;
 
         private State _showUpgrade;
         private State _upgradeSelected;
         private bool _isUpgraded = false;
+
+        private string _healthMsg => $"Increase overall health by {_health.ToString()}.";
+        private string _damageMsg => $"Increase overall damage by {_dmg.ToString()}.";
+        private string _speedMsg => $"Increase speed and turn speed by {_speed.ToString()}.";
 
         private void Start()
         {
@@ -29,6 +37,10 @@ namespace PlanetCracker.UIs
 
             _stageStageManager.AddState(StageStateManager.ShowUpgrade, ref _showUpgrade);
             _stageStageManager.AddState(StageStateManager.UpgradeSelected, ref _upgradeSelected);
+
+            _healthText.text = _healthMsg;
+            _damageText.text = _damageMsg;
+            _speedText.text = _speedMsg;
         }
 
         private bool IsUpgradeSelected() => _isUpgraded;
